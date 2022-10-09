92f79b593f8f2fe91d587a217c1154b756692202
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-09 16:09:57 +0300

    frontend<>adding inputs in the login signup form

--- frontend/system-react/src/FormSignup.js
+++ frontend/system-react/src/FormSignup.js
@@ -3,7 +3,14 @@ import React from 'react'
 const FormSignup = () => {
   return (
     <div className="form-content-right">
-        
+       <form className="form">
+            <h1>Get started</h1>
+            <div class="form-inputs">
+              <label for="username"></label>
+              <input type="text"
+                class="form-input" name="username" id=""  placeholder="Enter your username" />
+            </div>
+       </form>
     </div>
 
   )

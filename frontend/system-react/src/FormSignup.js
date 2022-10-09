355f82200be5863fa168f874520a36e4f9400d3e
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-09 21:08:08 +0300

    frontend<>making sure that the email and password are valid

--- frontend/system-react/src/FormSignup.js
+++ frontend/system-react/src/FormSignup.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import useForm from './Useform'
 
 const FormSignup = () => {
+    const{handleChange,Values} = useForm();
+
   return (
     <div className="form-content-right">
        <form className="form">
@@ -8,22 +11,22 @@ const FormSignup = () => {
             <div class="form-inputs">
               <label for="username">Username</label>
               <input type="text"
-                class="form-input" name="username" id="username"  placeholder="Enter your username" />
+                class="form-input" name="username" id="username"  placeholder="Enter your username"  value={Values.username} onChange={handleChange}/>
             </div>
             <div class="form-inputs">
               <label for="email">Email</label>
-              <input type="text"
-                class="form-input" name="email" id="email"  placeholder="Enter your email" />
+              <input type="email"
+                class="form-input" name="email" id="email"  placeholder="Enter your email" value={Values.email} onChange={handleChange}/>
             </div>
             <div class="form-inputs">
               <label for="password">Password</label>
-              <input type="text"
-                class="form-input" name="password" id="password"  placeholder="Enter your password" />
+              <input type="password"
+                class="form-input" name="password" id="password"  placeholder="Enter your password" value={Values.password} onChange={handleChange}/>
             </div>
             <div class="form-inputs">
               <label for="password2">Confirm Password</label>
-              <input type="text"
-                class="form-input" name="password2" id="password2"  placeholder="Confirm password" />
+              <input type="password"
+                class="form-input" name="password2" id="password2"  placeholder="Confirm password" value={Values.password2} onChange={handleChange}/>
             </div>
             <button className='form-input-btn' type='submit'>Sign up</button>
             <span className='form-input-login'>Already have an account?Login<a href='' >here</a></span>
--- frontend/system-react/src/Useform.js
+++ frontend/system-react/src/Useform.js
@@ -1,2 +1,27 @@
 import { useState , useEffect } from "react";
 
+const useForm = () => {
+    const[Values , setValues] = useState({
+        username:'',
+        email:'',
+        password:'',
+        password2:''
+    })
+    // state the errors 
+    const[errors,setErrors] = useState({})
+
+
+    // whenever changing something it update the values
+    const handleChange = e =>{
+        const { name , Value } = e.target
+        setValues({
+            //spreading in the values
+            ...Values,
+            //targeting the name inputs
+            [name]:Value
+        })
+    }
+    return {handleChange , Values};
+};
+
+export default useForm;

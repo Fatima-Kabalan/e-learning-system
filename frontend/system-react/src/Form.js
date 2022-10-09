e1b49adf069d61c9f8a8bcf00770dbc1254670cb
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-09 23:25:51 +0300

    frontend<>adding css to the login signup page

--- frontend/system-react/src/Form.js
+++ frontend/system-react/src/Form.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import FormSignup from './FormSignup'
 import FormSuccess from './FormSuccess'
+import './Form.css';
 
 const Form = () => {
   const[isSubmitted, setIsSubmitted] = useState(false)
@@ -10,13 +11,16 @@ const Form = () => {
     setIsSubmitted(true);
   }
   return (
-    <div>
-        <FormSignup/>
-        // if it's not submitted I wanna display Formsignup 
-        {!isSubmitted ? <FormSignup submitForm = {submitForm} /> : <FormSuccess />} 
-        // else if I do already submitted I wanna pass in FormSuccess
-    </div>
-  )
-}
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>x</span>
+        <div className='form-content-left'>
+          <img src="./images/img.jpg" alt="e-learning-pic" className='form-img' />
+        </div>
+        {!isSubmitted ? (<FormSignup submitForm = {submitForm} />) : (<FormSuccess />)} 
+      </div>
+    </>
+  );
+};
 
 export default Form
---
+++ frontend/system-react/src/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+     <div className = 'form-content-right'>
+        <div className='form-success'>
+            <h1>we have recieved your request</h1>
+        </div>
+     </div>
+  )
+}
+
+export default FormSuccess

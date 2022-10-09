b0b4d06a08b2a46cd0655d8ac91a8bf6aa1e904b
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-09 20:25:50 +0300

    frontend<>adding signup button and creating a hook

--- frontend/system-react/src/FormSignup.js
+++ frontend/system-react/src/FormSignup.js
@@ -6,12 +6,30 @@ const FormSignup = () => {
        <form className="form">
             <h1>Get started</h1>
             <div class="form-inputs">
-              <label for="username"></label>
+              <label for="username">Username</label>
               <input type="text"
-                class="form-input" name="username" id=""  placeholder="Enter your username" />
+                class="form-input" name="username" id="username"  placeholder="Enter your username" />
             </div>
+            <div class="form-inputs">
+              <label for="email">Email</label>
+              <input type="text"
+                class="form-input" name="email" id="email"  placeholder="Enter your email" />
+            </div>
+            <div class="form-inputs">
+              <label for="password">Password</label>
+              <input type="text"
+                class="form-input" name="password" id="password"  placeholder="Enter your password" />
+            </div>
+            <div class="form-inputs">
+              <label for="password2">Confirm Password</label>
+              <input type="text"
+                class="form-input" name="password2" id="password2"  placeholder="Confirm password" />
+            </div>
+            <button className='form-input-btn' type='submit'>Sign up</button>
+            <span className='form-input-login'>Already have an account?Login<a href='' >here</a></span>
        </form>
     </div>
+    
 
   )
 }

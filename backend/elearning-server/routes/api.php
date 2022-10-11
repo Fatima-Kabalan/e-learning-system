b14e2a8d310751ff7e51f02a97c37e9aca530e11
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-11 09:17:35 +0300

    fixing the routes

--- backend/elearning-server/routes/api.php
+++ backend/elearning-server/routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
+Route::post("/login", [AuthController::class, "login"])->name("login");
+
+Route::group(["prefix"=> "v1"], function(){
+
+    Route::group(["middleware" => "auth:api"], function(){
+        Route::post("/getUsers", [UserController::class, "getUsers"])->name("getUsers"); 
+    });
+
 });
